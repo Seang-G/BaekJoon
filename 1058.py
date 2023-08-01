5692d9f7055048ed926e122bd9d7ee0c9e108a6b
Seang-G <mgw7730@gmail.com>
2023-08-01 11:47:22 +0900

    Create 1058.py

---
+++ 1058.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+N = int(input())
+
+people = []
+people_tf = []
+for _ in range(N):
+  people.append(input())
+  people_tf.append([0 for _ in range(N)])
+
+for i, human in enumerate(people):
+  for j, friend in enumerate(human):
+    if friend == "Y":
+      people_tf[i][j] = 1
+      for k, ffriend in enumerate(people[j]):
+        if ffriend == "Y":
+          people_tf[i][k] = 1
+
+for i in range(N):
+  people_tf[i][i] = 0
+
+print(max(list(map(lambda x: Counter(x)[1], people_tf))))
